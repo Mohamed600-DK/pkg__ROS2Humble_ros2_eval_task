3627be711ab5c77433013da9084c7284221e6b79
Mohamed Ahmed Hussein <mohamedahmed_DK@hotmail.com>
2025-08-30 18:19:30 +0300

    Refactor GazeboUtils to include model list service and improve entity existence checks

--- include/gazebo_utils.hpp
+++ include/gazebo_utils.hpp
@@ -6,6 +6,7 @@
 #include "geometry_msgs/msg/pose.hpp"
 #include "gazebo_msgs/srv/spawn_entity.hpp"
 #include "gazebo_msgs/srv/delete_entity.hpp"
+#include "gazebo_msgs/srv/get_model_list.hpp"
 
 
 
@@ -14,11 +15,14 @@
 class GazeboUtils
 {
 private:
-    rclcpp::Node::SharedPtr ptr_gazebo_node_;
+    std::shared_ptr<rclcpp::Node> ptr_gazebo_node_;
     rclcpp::Client<gazebo_msgs::srv::SpawnEntity>::SharedPtr ptr_spawn_;
     rclcpp::Client<gazebo_msgs::srv::DeleteEntity>::SharedPtr ptr_delete_;
+    rclcpp::Client<gazebo_msgs::srv::GetModelList>::SharedPtr ptr_get_model_list_;
+
+    bool priv__check_entity_exists(const std::string &model_name);
 public:
-  GazeboUtils(rclcpp::Node::SharedPtr node);
+  GazeboUtils(std::shared_ptr<rclcpp::Node> node_);
 
   bool spawn_model(const std::string &model_name,
                    const std::string &xml,
--- lib/gazebo_utils.cpp
+++ lib/gazebo_utils.cpp
@@ -2,24 +2,61 @@
 
 constexpr const char *__SWAP_SERVICE_NAME = (const char *)"spawn_entity";
 constexpr const char *__DELETE_SERVICE_NAME = (const char *)"delete_entity";
+constexpr const char *__GET_MODELS_SERVICE_NAME = (const char *)"get_model_list";
 constexpr const uint8_t __REQUEST_DELAY_TIME = 5;
 constexpr const uint8_t __SERVICE_CHECK_DELAY_TIME = 2;
 
-GazeboUtils::GazeboUtils(rclcpp::Node::SharedPtr node)
-    : ptr_gazebo_node_(node)
+
+
+GazeboUtils::GazeboUtils(std::shared_ptr<rclcpp::Node> node_)
+    : ptr_gazebo_node_(node_)
 {
-    ptr_spawn_ = node->create_client<gazebo_msgs::srv::SpawnEntity>(__SWAP_SERVICE_NAME);
-    ptr_delete_ = node->create_client<gazebo_msgs::srv::DeleteEntity>(__DELETE_SERVICE_NAME);
-    ptr_delete_ = node->create_client<gazebo_msgs::srv::DeleteEntity>(__DELETE_SERVICE_NAME);
+    ptr_spawn_ = ptr_gazebo_node_->create_client<gazebo_msgs::srv::SpawnEntity>(__SWAP_SERVICE_NAME);
+    ptr_delete_ = ptr_gazebo_node_->create_client<gazebo_msgs::srv::DeleteEntity>(__DELETE_SERVICE_NAME);
+    ptr_get_model_list_ = ptr_gazebo_node_->create_client<gazebo_msgs::srv::GetModelList>(__GET_MODELS_SERVICE_NAME);
 }
+
+bool GazeboUtils::priv__check_entity_exists(const std::string &model_name)
+{
+    auto request = std::make_shared<gazebo_msgs::srv::GetModelList::Request>();
+
+    while (ptr_get_model_list_ != NULL && !ptr_get_model_list_->wait_for_service(std::chrono::seconds(__SERVICE_CHECK_DELAY_TIME)))
+    {
+        if (!rclcpp::ok())
+        {
+            RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Interrupted while waiting for the service. Exiting.");
+            return false;
+        }
+        RCLCPP_WARN(ptr_gazebo_node_->get_logger(), "service not available, waiting again...");
+    }
+
+    auto getModelListRequestFuture = ptr_get_model_list_->async_send_request(request);
+    auto status = getModelListRequestFuture.wait_for(std::chrono::seconds(__REQUEST_DELAY_TIME));
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(ptr_gazebo_node_, getModelListRequestFuture) == rclcpp::FutureReturnCode::SUCCESS)
+    {
+        for (const auto &name : getModelListRequestFuture.get()->model_names) {
+            if (name == model_name) {
+                return true;
+            }
+        }
+    } else {
+        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Failed to call service get_model_list");
+    }
+    return false;
+}
+
+
+
+
 
 bool GazeboUtils::spawn_model(const std::string &model_name,
                               const std::string &xml,
                               const geometry_msgs::msg::Pose &pose)
 {
-    if (ptr_spawn_ != NULL && !ptr_spawn_->wait_for_service(std::chrono::seconds(__SERVICE_CHECK_DELAY_TIME)))
+    if (priv__check_entity_exists(model_name))
     {
-        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Service spawn_entity not available");
+        RCLCPP_WARN(ptr_gazebo_node_->get_logger(), "Entity %s already exists", model_name.c_str());
         return false;
     }
     auto request = std::make_shared<gazebo_msgs::srv::SpawnEntity::Request>();
@@ -28,45 +65,58 @@ bool GazeboUtils::spawn_model(const std::string &model_name,
     request->robot_namespace = model_name;
     request->initial_pose = pose;
     request->reference_frame = "world";
+    
+
+    while (ptr_spawn_ != NULL && !ptr_spawn_->wait_for_service(std::chrono::seconds(__SERVICE_CHECK_DELAY_TIME)))
+    {
+        if (!rclcpp::ok())
+        {
+            RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Interrupted while waiting for the service. Exiting.");
+            return false;
+        }
+        RCLCPP_WARN(ptr_gazebo_node_->get_logger(), "service not available, waiting again...");
+    }
+
 
     auto spawnRequestFuture = ptr_spawn_->async_send_request(request);
-    auto status = spawnRequestFuture.wait_for(std::chrono::seconds(__REQUEST_DELAY_TIME));
-
-    if (status == std::future_status::ready)
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(ptr_gazebo_node_, spawnRequestFuture) ==
+        rclcpp::FutureReturnCode::SUCCESS)
     {
         auto response = spawnRequestFuture.get();
         RCLCPP_INFO(ptr_gazebo_node_->get_logger(), "Spawn response: %s", response->status_message.c_str());
-        return (response->success);
+        return response->success;
+    } else {
+        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Failed to call service spawn_entity");
     }
-    else
-    {
-        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Failed to call %s service", __SWAP_SERVICE_NAME);
-        return false;
-    }
+    return false;
 }
 
 bool GazeboUtils::delete_model(const std::string &model_name)
 {
-    if (ptr_delete_ != NULL && !ptr_delete_->wait_for_service(std::chrono::seconds(__SERVICE_CHECK_DELAY_TIME)))
-    {
-        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Service delete_entity not available");
-        return false;
-    }
+
     auto request = std::make_shared<gazebo_msgs::srv::DeleteEntity::Request>();
     request->name = model_name;
+    while (ptr_delete_ != NULL && !ptr_delete_->wait_for_service(std::chrono::seconds(__SERVICE_CHECK_DELAY_TIME)))
+    {
+        if (!rclcpp::ok())
+        {
+            RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Interrupted while waiting for the service. Exiting.");
+            return false;
+        }
+        RCLCPP_WARN(ptr_gazebo_node_->get_logger(), "service not available, waiting again...");
+    }
 
     auto deleteRequestFuture = ptr_delete_->async_send_request(request);
-    auto status = deleteRequestFuture.wait_for(std::chrono::seconds(__REQUEST_DELAY_TIME));
-
-    if (status == std::future_status::ready)
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(ptr_gazebo_node_, deleteRequestFuture) ==
+        rclcpp::FutureReturnCode::SUCCESS)
     {
         auto response = deleteRequestFuture.get();
         RCLCPP_INFO(ptr_gazebo_node_->get_logger(), "Delete response: %s", response->status_message.c_str());
-        return (response->success);
+        return response->success;
+    } else {
+        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Failed to call service delete_entity");
     }
-    else
-    {
-        RCLCPP_ERROR(ptr_gazebo_node_->get_logger(), "Failed to call %s service", __DELETE_SERVICE_NAME);
-        return false;
-    }
+    return false;
 }
